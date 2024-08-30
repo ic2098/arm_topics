f0bc20978c89e28c017b9f878e70853dc865149f
ic2098 <30567506+ic2098@users.noreply.github.com>
2024-08-30 14:47:48 +0800

    Add files via upload
    Original version with no change, V0.

---
+++ ddr_test_latest/Debug/src/xmt_read_eye.d
@@ -0,0 +1,58 @@
+src/xmt_read_eye.o src/xmt_read_eye.o: ../src/xmt_read_eye.c \
+ ../src/xmt_common.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xil_printf.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xil_types.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xparameters.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xparameters_ps.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/bspconfig.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xil_types.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xstatus.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xil_assert.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xil_cache.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xil_io.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xil_printf.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xpseudo_asm.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xreg_cortexa53.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xpseudo_asm_gcc.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xil_exception.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xpseudo_asm.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xdebug.h \
+ ../../ddr_test_bsp/psu_cortexa53_0/include/xtime_l.h
+
+../src/xmt_common.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xil_printf.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xil_types.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xparameters.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xparameters_ps.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/bspconfig.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xil_types.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xstatus.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xil_assert.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xil_cache.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xil_io.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xil_printf.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xpseudo_asm.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xreg_cortexa53.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xpseudo_asm_gcc.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xil_exception.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xpseudo_asm.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xdebug.h:
+
+../../ddr_test_bsp/psu_cortexa53_0/include/xtime_l.h:
